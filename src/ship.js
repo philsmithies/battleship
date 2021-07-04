87a858391af31448de90c9106f516dfcd731f5e5
Phil Smithies <philsmithies@gmail.com>
2021-07-04 11:48:54 +0200

    hit function and body

--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,19 @@
-const Ship = () => {
-  let length = 0
+const Ship = (l) => {
+  let length = l
+  let body = []
+
+  for(i = 0; i < length; i++){
+    body.push('-')
+  }
+
+  function hit(value) {
+    body[value] = 'X'
+  }
 
   return {
-    get length() {return length}
+    hit,
+    get length() {return length},
+    get body() {return body}
   }
 }
 
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,9 +1,16 @@
 const Ship = require('../src/ship')
-
-const newShip = Ship()
+const newShip = Ship(3)
 
 describe('ship initialisation', () => {
-  test('it returns with a default value', () =>{
-    expect(newShip.length).toBe(0)
+  test('it can be given a length', () =>{
+    expect(newShip.length).toBe(3)
+  })
+  test('it can store whether it has been hit or not', () =>{
+    expect(newShip.body).toEqual(['-', '-', '-']) 
+  })
+  test('it can have a hit placed upon it', () =>{
+    expect(newShip.body).toEqual(['-', '-', '-']) 
+    expect(newShip.hit(0))
+    expect(newShip.body).toEqual(['X', '-', '-']) 
   })
 })
